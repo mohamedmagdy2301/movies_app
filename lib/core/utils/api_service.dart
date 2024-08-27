5ed60e18e1eefb071acf929324513125718c057d
Mohamed magdy <mohammedmego15@gmail.com>
2024-08-27 23:25:14 +0300

    create api service

--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -1,12 +1,14 @@
-// import 'package:dio/dio.dart';
+import 'package:dio/dio.dart';
 
-// class ApiService {
-//   ApiService(this.dio);
-//   final String _baseUrl =
-//       'https://www.googleapis.com/books/v1/volumes?Filtering=free-ebooks';
-//   Dio dio;
-//   Future<dynamic> get(String endpoint) async {
-//     final response = await dio.get("$_baseUrl$endpoint");
-//     return response.data;
-//   }
-// }
+class ApiService {
+  ApiService(this.dio);
+  final String _baseUrl = 'https://api.themoviedb.org/3/movie/';
+  final String _apiKey = 'dcb310be2ecda576819e5025740aa1e3';
+  Dio dio;
+  Future<dynamic> get(String endpoint) async {
+    final response =
+        await dio.get("$_baseUrl$endpoint?api_key=$_apiKey&page=1");
+    return response.data;
+  }
+}
+// /popular
