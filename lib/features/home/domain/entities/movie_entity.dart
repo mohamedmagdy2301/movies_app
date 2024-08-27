91b8bbc675a598c8e39a0ebac177913dd3eb49d3
Mohamed magdy <mohammedmego15@gmail.com>
2024-08-27 05:13:10 +0300

    create Movie Entity

---
+++ lib/features/home/domain/entities/movie_entity.dart
@@ -0,0 +1,27 @@
+class MovieEntity {
+  final int id;
+  final String title;
+  final String overview;
+  final String posterPath;
+  final String backdropPath;
+  final double voteAverage;
+  final int voteCount;
+  final String releaseDate;
+  final bool isFavorite;
+  final int popularity;
+
+  MovieEntity(
+      {required this.id,
+      required this.title,
+      required this.overview,
+      required this.posterPath,
+      required this.backdropPath,
+      required this.voteAverage,
+      required this.voteCount,
+      required this.releaseDate,
+      required this.isFavorite,
+      required this.popularity});
+
+  // String get posterImageUrl => 'https://image.tmdb.org/t/p/w500$posterPath';
+  // String get backdropImageUrl => 'https://image.tmdb.org/t/p/w500$backdropPath';
+}
