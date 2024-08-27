9213778740b12577cc2aded870cb582df1c7786f
Mohamed magdy <mohammedmego15@gmail.com>
2024-08-28 01:44:13 +0300

    setup Serice Locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:movies_app/core/constant.dart';
+import 'package:movies_app/core/utils/service_locator.dart';
 import 'package:movies_app/features/home/domain/entities/movie_entity.dart';
 import 'package:movies_app/movies_app.dart';
 
 void main() async {
   runApp(const MovieApp());
-
+  setupSericeLocator();
   await Hive.initFlutter();
   Hive.registerAdapter(MovieEntityAdapter());
   await Hive.openBox<MovieEntity>(kMoviesBox);
