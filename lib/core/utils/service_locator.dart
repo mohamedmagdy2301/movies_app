aa4f07c78c81514fc02e6c012342b8cc32ecb090
Mohamed magdy <mohammedmego15@gmail.com>
2024-08-28 01:21:39 +0300

    setup Serice Locator

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -1,24 +1,27 @@
-// import 'package:bookly/core/utils/api_service.dart';
-// import 'package:bookly/features/home/data/repo/home_repo_impl.dart';
-// import 'package:bookly/features/search/data/repo/search_repo_impl.dart';
-// import 'package:dio/dio.dart';
-// import 'package:get_it/get_it.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:movies_app/core/utils/api_service.dart';
+import 'package:movies_app/features/home/data/data%20sources/local_data_sources_home.dart';
+import 'package:movies_app/features/home/data/data%20sources/remote_data_sources_home.dart';
+import 'package:movies_app/features/home/data/repo/home_repo_impl.dart';
+import 'package:movies_app/features/home/domain/use%20case/fetch_movies_use_case.dart';
 
-// final getIt = GetIt.instance;
+final getIt = GetIt.instance;
 
-// void setupSericeLocator() {
-//   getIt.registerSingleton<HomeRepoImpl>(
-//     HomeRepoImpl(
-//       ApiService(
-//         Dio(),
-//       ),
-//     ),
-//   );
-//   getIt.registerSingleton<SearchRepoImpl>(
-//     SearchRepoImpl(
-//       ApiService(
-//         Dio(),
-//       ),
-//     ),
-//   );
-// }
+void setupSericeLocator() {
+  getIt.registerSingleton<RemoteDataSourcesHomeImpl>(
+    RemoteDataSourcesHomeImpl(
+      apiService: ApiService(
+        Dio(),
+      ),
+    ),
+  );
+  getIt.registerSingleton<FetchMoviesUseCase>(
+    FetchMoviesUseCase(
+      homeRepoImpl: HomeRepoImpl(
+        remoteDataSourcesHomeImpl: getIt.get<RemoteDataSourcesHomeImpl>(),
+        localDataSourcesHomeImpl: LocalDataSourcesHomeImpl(),
+      ),
+    ),
+  );
+}
