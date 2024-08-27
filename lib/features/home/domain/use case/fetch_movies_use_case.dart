bc00815b9d60acd3f0291f4fb15c90fb324d66bd
Mohamed magdy <mohammedmego15@gmail.com>
2024-08-28 01:25:13 +0300

    remove unuse import

--- lib/features/home/domain/use case/fetch_movies_use_case.dart
+++ lib/features/home/domain/use case/fetch_movies_use_case.dart
@@ -3,7 +3,6 @@ import 'package:movies_app/core/errors/failure.dart';
 import 'package:movies_app/core/utils/use_case.dart';
 import 'package:movies_app/features/home/data/repo/home_repo_impl.dart';
 import 'package:movies_app/features/home/domain/entities/movie_entity.dart';
-import 'package:movies_app/features/home/domain/repo/home_repo.dart';
 
 class FetchMoviesUseCase implements UseCase<List<MovieEntity>, NoParams> {
   final HomeRepoImpl homeRepoImpl;
