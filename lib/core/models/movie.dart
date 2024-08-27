b1ef7f7220ff4358a096b678f908e942af0947e2
Mohamed magdy <mohammedmego15@gmail.com>
2024-08-27 05:41:01 +0300

    create Movie Model

---
+++ lib/core/models/movie.dart
@@ -0,0 +1,89 @@
+import 'package:equatable/equatable.dart';
+
+class MovieModel extends Equatable {
+  final bool? adult;
+  final String? backdropPath;
+  final List<int>? genreIds;
+  final int? id;
+  final String? originalLanguage;
+  final String? originalTitle;
+  final String? overview;
+  final double? popularity;
+  final String? posterPath;
+  final String? releaseDate;
+  final String? title;
+  final bool? video;
+  final double? voteAverage;
+  final int? voteCount;
+
+  const MovieModel({
+    this.adult,
+    this.backdropPath,
+    this.genreIds,
+    this.id,
+    this.originalLanguage,
+    this.originalTitle,
+    this.overview,
+    this.popularity,
+    this.posterPath,
+    this.releaseDate,
+    this.title,
+    this.video,
+    this.voteAverage,
+    this.voteCount,
+  });
+
+  factory MovieModel.fromJson(Map<String, dynamic> json) => MovieModel(
+        adult: json['adult'] as bool?,
+        backdropPath: json['backdrop_path'] as String?,
+        genreIds: json['genre_ids'] as List<int>?,
+        id: json['id'] as int?,
+        originalLanguage: json['original_language'] as String?,
+        originalTitle: json['original_title'] as String?,
+        overview: json['overview'] as String?,
+        popularity: (json['popularity'] as num?)?.toDouble(),
+        posterPath: json['poster_path'] as String?,
+        releaseDate: json['release_date'] as String?,
+        title: json['title'] as String?,
+        video: json['video'] as bool?,
+        voteAverage: (json['vote_average'] as num?)?.toDouble(),
+        voteCount: json['vote_count'] as int?,
+      );
+
+  Map<String, dynamic> toJson() => {
+        'adult': adult,
+        'backdrop_path': backdropPath,
+        'genre_ids': genreIds,
+        'id': id,
+        'original_language': originalLanguage,
+        'original_title': originalTitle,
+        'overview': overview,
+        'popularity': popularity,
+        'poster_path': posterPath,
+        'release_date': releaseDate,
+        'title': title,
+        'video': video,
+        'vote_average': voteAverage,
+        'vote_count': voteCount,
+      };
+
+  @override
+  List<Object?> get props {
+    return [
+      adult,
+      backdropPath,
+      genreIds,
+      id,
+      originalLanguage,
+      originalTitle,
+      overview,
+      popularity,
+      posterPath,
+      releaseDate,
+      title,
+      video,
+      voteAverage,
+      voteCount,
+    ];
+  }
+}
