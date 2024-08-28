d21bcd4e7b833c8551e7b3821218fdcf16393310
Mohamed magdy <mohammedmego15@gmail.com>
2024-08-28 03:29:38 +0300

    create Shimmer placeholder Item Feature Movies

--- lib/features/home/presentation/view/widgets/custom_item_featured_home.dart
+++ lib/features/home/presentation/view/widgets/custom_item_featured_home.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:movies_app/features/home/domain/entities/movie_entity.dart';
+import 'package:shimmer/shimmer.dart';
 
 class CustomItemFeaturedHome extends StatelessWidget {
   const CustomItemFeaturedHome({super.key, required this.movieImage});
@@ -8,15 +9,24 @@ class CustomItemFeaturedHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-        aspectRatio: 2.7 / 4,
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(15),
-          child: CachedNetworkImage(
-            imageUrl:
-                "https://image.tmdb.org/t/p/w500${movieImage.posterPath ?? "/yDHYTfA3R0jFYba16jBB1ef8oIt.jpg"}",
-            fit: BoxFit.fill,
-          ),
-        ));
+    return movieImage.posterPath == null
+        ? const SizedBox()
+        : AspectRatio(
+            aspectRatio: 2.7 / 4,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: CachedNetworkImage(
+                imageUrl:
+                    "https://image.tmdb.org/t/p/w500${movieImage.posterPath}",
+                placeholder: (context, url) => Shimmer.fromColors(
+                  enabled: true,
+                  baseColor: const Color.fromARGB(136, 92, 92, 92),
+                  highlightColor: const Color.fromARGB(149, 116, 116, 116),
+                  child: Container(color: Colors.red),
+                ),
+                fit: BoxFit.fill,
+              ),
+            ),
+          );
   }
 }
