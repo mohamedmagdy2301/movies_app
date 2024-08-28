f1da5e531e89befadcba1cab5fc39237fe4b5b7e
Mohamed magdy <mohammedmego15@gmail.com>
2024-08-28 11:19:55 +0300

    update url api services

--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -6,9 +6,8 @@ class ApiService {
   final String _apiKey = 'dcb310be2ecda576819e5025740aa1e3';
   Dio dio;
   Future<dynamic> get(String endpoint) async {
-    final response = await dio.get(
-        "https://api.themoviedb.org/3/movie/popular?api_key=dcb310be2ecda576819e5025740aa1e3&page=1");
+    final response =
+        await dio.get("$_baseUrl$endpoint?api_key=$_apiKey&page=1");
     return response.data;
   }
 }
-// /popular
